ba4ed53b6c1fdacd81dabc0749550c7f26e98233
delozix <delozix@gmail.com>
2023-08-12 12:14:14 +0300

    Add category class

---
+++ FirstWebsite/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstWebsite.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public int CategoryName { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
